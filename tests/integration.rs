78bff954e5eee8338515ae08e1a9445b9b19c712
Michael J. Curry <currymj@fastmail.fm>
2016-09-08 20:23:02 -0400

    added another integration test

--- tests/integration.rs
+++ tests/integration.rs
@@ -10,7 +10,30 @@ fn build_table_from_mnist() {
     let vec_length = mnist_data[0].len();
 
     let num_hashes = 10;
-    let hash_boxes: Vec<_> = (1..num_hashes).map(|_| o2lsh::hashes::get_hash_closure(vec_length, 1.0)).collect();
+    let hash_boxes: Vec<_> = (1..num_hashes).map(|| o2lsh::hashes::get_hash_closure(vec_length, 1.0)).collect();
 
     let hash_table: o2lsh::table::LSHTable<Vec<f64>, Fn(&Vec<f64>) -> f64> = o2lsh::table::LSHTable::new_build(&mnist_data, &hash_boxes);
+}
+
+#[test]
+fn build_table_from_mnist_and_query() {
+    let mnist_data = match o2lsh::util::get_mnist_vector("mnist1k.dts") {
+        Ok(v) => v,
+        Err(reason) => panic!("couldnt open because {}", reason)
+    };
+
+    let vec_length = mnist_data[0].len();
+
+    let num_hashes = 10;
+    let hash_boxes: Vec<_> = (1..num_hashes).map(|| o2lsh::hashes::get_hash_closure(vec_length, 1.0)).collect();
+
+    let hash_table: o2lsh::table::LSHTable<Vec<f64>, Fn(&Vec<f64>) -> f64> = o2lsh::table::LSHTable::new_build(&mnist_data, &hash_boxes);
+    let mnist_q = match o2lsh::util::get_mnist_vector("mnist1k.q") {
+        Ok(v) => v,
+        Err(reason) => panic!("couldnt open because {}", reason)
+    };
+
+    for vec in mnist_q.iter() {
+        hash_table.query_vec(&vec);
+    }
 }
