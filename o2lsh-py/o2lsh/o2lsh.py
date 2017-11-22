c9304efb9c8536062ff56041572df71fe08a44cb
Michael J. Curry <curry@cs.umd.edu>
2017-11-22 16:03:09 -0500

    at last, there's a decent solution for providing python interfaces to rust code

---
+++ o2lsh-py/o2lsh/o2lsh.py
@@ -0,0 +1,5 @@
+from ._o2lsh import MyClass
+
+def run_class(input_string):
+    mc = MyClass()
+    mc.hello(input_string)
--- o2lsh-py/src/lib.rs
+++ o2lsh-py/src/lib.rs
@@ -1,6 +1,28 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
+#![feature(proc_macro, specialization, const_fn)]
+#![feature(const_fn, const_align_of, const_size_of, const_ptr_null, const_ptr_null_mut)]
+extern crate pyo3;
+use pyo3::prelude::*;
+
+#[py::class]
+struct MyClass {
+}
+
+#[py::methods]
+impl MyClass {
+
+   #[new]
+   fn __new__(obj: &PyRawObject) -> PyResult<()> {
+       obj.init(|_x| MyClass {})
+   }
+
+   fn hello(&self, name: String) -> PyResult<String> {
+       Ok(name)
+   }
+}
+
+#[py::modinit(_o2lsh)]
+fn init_mod(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<MyClass>()?;
+
+    Ok(())
 }
